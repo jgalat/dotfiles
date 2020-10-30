692e4a8566c806d076fbc828d865e34a023ed85c
Jorge Galat <jrgglt@gmail.com>
2020-10-30 19:33:02 -0300

    update dmenu to 5.0

--- suckless/dmenu/config.h
+++ suckless/dmenu/config.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Inconsolata:size=12"
+	"Liberation Mono:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char col_gray1[]  = "#222222";
