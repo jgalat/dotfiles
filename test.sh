f38cf66cd1cbf46b5836f347690d3b76f3936c32
Jorge Galat <jrgglt@gmail.com>
2022-07-15 19:03:19 -0300

    update test

--- test.sh
+++ test.sh
@@ -1,20 +1,10 @@
 #!/bin/sh
 
 root_scripts="install.sh test.sh"
-scripts="$(find home -follow -type f)"
+scripts="$(find home -follow -type f | xargs grep -E -l "/bin/bash|/bin/sh" | xargs)"
 
 if command -v "shellcheck" > /dev/null 2>&1; then
-  error_count=0
-  for script in $root_scripts $scripts; do
-    head="$(head -n 1 "${script}")"
-    if [ "${head}" = "#!/bin/sh" ] || [ "${head}" = "#!/bin/bash" ]; then
-      echo "shellcheck ${script}"
-      shellcheck "${script}"
-      error_count="$((error_count + $?))"
-    fi
-  done
-
-  [ "${error_count}" -eq 0 ]
+  shellcheck ${root_scripts} ${scripts}
 else
   echo "ShellCheck is required."
   false
