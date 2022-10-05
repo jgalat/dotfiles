1542f1c10d6a780535f403431c18f3a5c1682f70
Jorge Galat <jrgglt@gmail.com>
2022-10-05 18:41:08 -0300

    improve install

--- test.sh
+++ test.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 
-scripts="$(find home -follow -type f -exec xargs grep -E -l "/bin/bash|/bin/sh")"
-
-# if command -v "shellcheck" > /dev/null 2>&1; then
-  echo SORETE "${scripts}"
-  docker run --rm -v "$PWD:/mnt" koalaman/shellcheck:stable install.sh test.sh "${scripts}"
-# else
-#   echo "ShellCheck is required."
-#   false
-# fi
+# shellcheck disable=SC2046
+docker run --rm -v "$PWD:/mnt" koalaman/shellcheck:stable install.sh test.sh $( grep -E -l "/bin/bash|/bin/sh" -R home )
