36e6899411cb68c6e53b31dcf5e2cf78379cb474
Jorge Galat <jrgglt@gmail.com>
2026-08-21 09:49:50 -0300

    fix(dwm): route Steam Link to HDMI

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -27,7 +27,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "firefox",  NULL,       NULL,       1 << 2,       0,           -1 },
-	{ "zoom",     "zoom",     NULL,       0,            1,           -1 },
+	{ "SteamLink", NULL,      NULL,       0,            0,            1 },
 };
 
 /* layout(s) */
